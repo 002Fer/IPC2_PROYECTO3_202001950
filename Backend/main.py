1dbf066d82c57afab0f4e6dcef42ab83cecb9229
002fer <feralone86@gmail.com>
2023-05-03 23:39:11 -0600

    metodos en el servidor

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,5 @@
 from flask import Flask,request,Response,jsonify
+from proceso import xmlADiccionario,EscribirBasePalabras,unificarDiccionarios
 import xml.etree.ElementTree as ET
 import os 
 app = Flask(__name__)
@@ -10,6 +11,23 @@ def consulta():
     nombreUser=request.args.get('nombreUser')
     return jsonify({"nombre":nombreUser})
 
+@app.route("/procesarSolicitud",methods=['POST'])
+def Cargar():
+    if request.method=='POST':
+        ruta_archivo = 'diccionario.xml'
+        if os.path.exists(ruta_archivo):
+            tree = ET.parse(ruta_archivo)
+            root = tree.getroot()
+            xml_string = ET.tostring(root, encoding='utf8', method='xml').decode('utf8')
+            diccionario1=xmlADiccionario(request.data)
+            diccionario2=xmlADiccionario(xml_string)
+            diccionarioNuevo=unificarDiccionarios(diccionario1,diccionario2)
+            EscribirBasePalabras(diccionarioNuevo)
+        else:
+            EscribirBasePalabras(xmlADiccionario(request.data))
+        return "Ok"
+    else:
+        return "Wrong response"
 
     
 if __name__=='_main_':
